e069e0d381c198b0419cf4a48b0a19b85b2e249c
convivial-expression-6540 <jamaltbutler@gmail.com>
2019-05-12 22:01:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var specialGuest ="Neil DeGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "space.";
+var topic = "space";
 
-var conversation = `Let's talk about + ${topic}`;
+var conversation = `Let's talk about  ${topic} .`;
 
